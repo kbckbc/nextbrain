1cc41ffe6f0a00758f9558c11eadbda148a91f22
Byeongchan Gwak <kbckbc@gmail.com>
2022-07-10 23:07:35 -0500

    port adjust

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening 3000'));
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`listening at ${port}`));
 app.use(express.static('public'));
