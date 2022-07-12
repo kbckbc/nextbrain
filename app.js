d7a8b844b33509bccb4550bdecf1f884613c044a
Byeongchan Gwak <kbckbc@gmail.com>
2022-07-11 21:07:55 -0500

    mongodb try

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const nedb = require('nedb');
+
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening at ${port}`));
+const uri = process.env.MONGODB_URI;
+app.listen(port, () => {
+    console.log(`listening at ${port}`);
+    console.log('uri', uri);
+});
 
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
@@ -23,3 +28,4 @@ app.get('/state/ranking', (req, res) => {
         res.json(data);
     });
 });
+
