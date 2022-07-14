d7cde11201eb3a0f980ad5101784de75a9ac8092
Byeongchan Gwak <kbckbc@gmail.com>
2022-07-14 12:01:26 -0500

    add handlebars

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 // express
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 
+
+// handlebars
+const hbs = require('express-handlebars');
+app.engine( 'hbs', hbs.engine({
+    extname: 'hbs',
+    // defaultView: 'default',
+    // layoutsDir: __dirname + '/views/layouts/',
+    // partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'hbs');
+
+
+// router
 const state = require('./routes/state');
 app.use('/state', state);
 
+
+// start app
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening at ${port}`);
+});
+
+
+// home rendering
+app.get('/', (req, res) => {
+  res.render('home');
 });
