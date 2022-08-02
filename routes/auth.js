1d3bed5973fcc3c83875ffed5ae9e021eadfa648
Byeongchan Gwak <kbckbc@gmail.com>
2022-08-02 09:57:57 -0500

    apply mongo db for user login, sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const currPath = '';
 
 module.exports = function (passport) {
-  router.get(currPath + '/login', function(req, res, next) {
+  router.get('/login', function(req, res, next) {
     res.render('login');
   });
   
-  router.post(currPath + '/login/password', passport.authenticate('local', {
+  router.post('/login', passport.authenticate('local', {
     successRedirect: '/game/state',
     failureRedirect: '/auth/login'
   }));
   
-  router.get(currPath + '/signup', function(req, res, next) {
+  router.get('/signup', function(req, res, next) {
     res.render('signup');
   });
 
-  router.get(currPath + '/logout', function(req, res, next) {
+  router.post('/signup', function(req, res, next) {
+    console.log('auth', '/signup', 'req.body', req.body);
+
+    // prepare user data
+    let data = req.body;
+    data.time = Date.now();
+    
+
+    var bcrypt = require('bcryptjs');
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(data.password, salt, function(err, hash) {
+            data.password = hash;
+            delete data.password2;
+        });
+    });
+
+
+    // // mongo db
+    const { MongoClient } = require("mongodb");
+    const uri = process.env.MONGODB_URI || 'mongodb+srv://nextbrain:NsGzoib2VlmnKbTe@cluster0.swhee.mongodb.net/?retryWrites=true&w=majority';
+    const dbName = 'nextbrainDB';
+    const collName = 'user';
+
+    async function run() {
+      const mongoClient = new MongoClient(uri);
+      try {
+        const mongodb = mongoClient.db(dbName);
+        const coll = mongodb.collection(collName);
+        // create a document to insert
+        const result = await coll.insertOne(data);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      } finally {
+        await mongoClient.close();
+      }
+    }
+    run().catch(console.dir); 
+
+    res.redirect('/');
+  });
+
+  router.get('/logout', function(req, res, next) {
     console.log('auth', '/logout 1', req.user, 'req.session', req.session);
     if (req.session) {
       req.session.destroy();
